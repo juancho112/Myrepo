245e19c7c3f988a557c7fc945147f12c8f4d1e61
juancho112 <juanchopepe112@hotmail.com>
2019-03-15 12:02:01 -0500

    scripts php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,16 @@
+<center> Programacion avanzada</center>
+<?php
+    $nombre = "Peter";
+    $apellido = "Watson";
+    $edad = 30;
+    $salario = 800;
+
+
+    echo "<center>Bienvenidos</center>";
+    echo "Nombre".$nombre."<br>";
+    echo "Apellido".$apellido;
+    echo "<br>Edad".$edad;
+    echo "<br>Salario".$salario;
+
+ ?>
+<center> Fin del programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,18 @@
+<?php
+
+  $num1 = 10;
+  $num2 = 5;
+
+  $suma = $num1 + $num2;
+  $resta = $num1 - $num2;
+  $mult = $num1 * $num2;
+  $div = $num1 / $num2;
+
+  echo "<center>";
+  echo "las suma es". $suma;
+  echo "<br>la resta es". $resta;
+  echo "<br>la multiplicacion es". $mult;
+  echo "<br>la division es". $div;
+  echo "<center>";
+
+ ?>
