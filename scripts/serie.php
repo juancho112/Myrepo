12ff35d9b5a2c447e0d297c94ca52cb769546f3e
juancho112 <juanchopepe112@hotmail.com>
2019-04-01 19:35:37 -0500

    Adelanto taller

--- scripts/serie.php
+++ scripts/serie.php
@@ -24,8 +24,7 @@
            <center><input  type="submit" value="Calcular"></center>
 
        </form>
-    </body>
-</html>
+
 
 <?php
 
@@ -53,31 +52,24 @@
 
 
     echo "Numero :".$n1;
-    echo "<table border =1>";
+    echo "<table border =1 >";
+    echo "<tr>";
     $i=1;
     while ($i <= $n1) {
       echo "<tr><td>".$i."</td></tr>";
 
       $i++; //$i = $i}
     }
-    $i=$n1;
-    while ($i > 0) {
-      echo "<td>".$i."</td>";
-      $i--; //$i = $i
-    }
 
-
-    echo "</table>";
-
-
-    echo "Numero :".$n1;
-    echo "<table border =1>";
     $i=$n1;
     while ($i > 0) {
       echo "<tr><td>".$i."</td></tr>";
       $i--; //$i = $i
     }
+    echo "</tr>";
     echo "</table>";
+
+
 
     //1
     echo "<b> ------------------------------------------------------------------------------------------------------------</b>";
@@ -148,4 +140,117 @@
     echo "<br><b> ------------------------------------------------------------------------------------------------------------</b>";
     echo "<br><b>7) Escriba un Script en PHP que muestre por pantalla los múltiplos de 3 comprendidos en la serie del 1 al 100.</b><br>";
 
+    echo "<br><table border =1>";
+    for($i = 3; $i <= 100; $i +=3 )
+    echo "<td>".$i."</td>";
+    echo "</table>";
+
+    //8
+    echo "<br><b> ------------------------------------------------------------------------------------------------------------</b>";
+    echo "<br><b>8) Escriba un Script en PHP que genere aleatoriamente 10 números enteros positivos y los muestre por pantalla, y visualice además el
+          siguiente reporte en una tabla HTML:
+          <br>- Total de números pares generados.
+          <br>- Total de números impares generados</b><br>";
+
+    $par=0;
+    $impar=0;
+    echo "<br><table border =1>";
+    for ($i=1 ; $i<=10; $i++) {
+        $ale = rand(1,100);
+    if ($ale % 2==0) {
+        $par=$par+1;
+        }
+    else {
+            $impar=$impar+1;
+          }
+            echo "<td>".$ale."</td> ";
+              }
+      echo "</table>";
+
+      echo "<table border =1>";
+      echo "<br><tr><td> Total de números pares generados.</td><td> ".$par." </td></tr>";
+      echo "<tr> <td> Total de números impares generados</td><td> ".$impar." </td></tr>";
+      echo "</table>";
+
+      //9
+      echo "<br><b> ------------------------------------------------------------------------------------------------------------</b>";
+      echo "<br><b>9) Escriba un Script en PHP que genere aleatoriamente 10 números enteros positivos y negativos, y los  muestre  por  pantalla,  y  visualice además el siguiente reporte en una tabla HTML:
+            <br>-Total de números positivos pares generados.
+            <br>-Total de números positivos impares generados.
+            <br>-Total de números negativos pares generados.
+            <br>-Total de números negativos impares generados
+            </b><br>";
+
+      $par_pos=0;
+      $impar_pos=0;
+      $par_neg=0;
+      $impar_neg=0;
+
+      echo "<br><table border=1>";
+
+      for ($i=1;$i<=10;$i++) {
+          $ale = rand(-100,100);
+            echo "<td>".$ale." </td>";
+              if($ale > 0  &&  ($ale%2)==0){
+                  $par_pos=$par_pos+1;
+              }else if ($ale > 0  && (($ale % 3)==0 || ($ale % 3)!=0)) {
+                $impar_pos=$impar_pos+1;
+              }if($ale < 0  &&  ($ale % 2)==0){
+                $par_neg=$par_neg+1;
+                }else if ($ale < 0  && (($ale%3)==0 || ($ale % 3)!=0)) {
+                  $impar_neg=$impar_neg+1;
+              }
+        }
+        echo "</table>";
+
+        echo "<br><table border=1>";
+        echo "<td> Total de Numeros postivos pares: </td> <td>".$par_pos."</td>";
+        echo "<tr><td> Total de Numeros postivos impares: </td> <td>".$impar_pos."</td></tr>";
+        echo "<tr><td> Total de Numeros negativos pares: </td> <td>".$par_neg."</td></tr>";
+        echo "<tr><td> Total de Numeros negativos impares: </td> <td>".$impar_neg."</td></tr>";
+        echo "</table>";
+
+        //10
+        echo "<br><b> ------------------------------------------------------------------------------------------------------------</b>";
+        echo "<br><b>Scripts PHP con Formulario HTML5 </b><br>";
+        echo "<br><b>10) Escriba un Script en PHP que solicite por pantalla al usuario un número N, y de ese número ingresado se visualice la tabla de multiplicar respectiva con una serie de 1 a 10.
+        <br>(Haga uso de un solo archivo para crear formulario html y Script php). </b><br>";
+
+
+
+        $n2=$_POST ["num2"];
+        echo "<table border =1 align = center>";
+          for($h=1; $h<=10; $h++)
+          {
+            echo "<tr><td align=center>$n2</td>
+                      <td align=center>x</td>
+                      <td align=center>$h</td>
+                      <td align=center>=</td>
+                      <td align=center> ". ($h*$n2) . "</td>
+                  </tr>";
+          }
+          echo "</table> ";
+
 ?>
+
+
+       <form name=frm2 action="serie.php" method="POST">
+          <table border="1" align="center">
+            <tr>
+                <td align=center> <font face="Trebuchet MS" size="16" color="#627EC1">TABLA DE MULTIPLICAR</font> </td>
+            </tr>
+
+            <tr>
+               <td font face="Trebuchet MS" size="3" > Digite numero: <font color="codigocolor"></font> </font> </td>
+            </tr>
+
+            <tr>
+              <td> <input type="number" name="num2" placeholder="Digite numero" ><br></td>
+            </tr>
+
+          </table>
+            <center><input  type="submit" value="Calcular"></center>
+
+        </form>
+    </body>
+</html>
